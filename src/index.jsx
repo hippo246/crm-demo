264caead206eba4b169942573b4a63f3beff8ddd
hippo246 <rahil.tahir01@gmail.com>
2026-05-16 02:35:11 +0530

    rename index.js to index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,8 @@
+# Open index.html and change this line:
+# <script type="module" src="/src/main.jsx"></script>
+# TO:
+# <script type="module" src="/src/index.js"></script>
+
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
