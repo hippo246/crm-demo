b6d6f67739043045bbbde3b7704a5af6b93c769c
hippo246 <rahil.tahir01@gmail.com>
2026-05-16 02:40:44 +0530

    fix corrupted index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,6 @@
-# Open index.html and change this line:
-# <script type="module" src="/src/main.jsx"></script>
-# TO:
-# <script type="module" src="/src/index.js"></script>
-
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-
 const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
